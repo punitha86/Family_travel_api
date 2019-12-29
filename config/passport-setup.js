44522073d519d6890927bff1d104059092318328
punitha86 <punith.job@gmail.com>
2019-12-29 11:31:36 -0800

    setting up keys

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,2 +1,12 @@
 const passport = require('passport');
 const GoogleStratergy = require('passport-google-oauth20');
+const keys = require('./keys.js');
+
+passport.use(new GoogleStratergy({
+  //options for GoogleStratergy
+clientID:keys.google.clientID,
+clientSecret:keys.google.clientSecret
+},() => {
+  //pass port call back function
+})
+)
