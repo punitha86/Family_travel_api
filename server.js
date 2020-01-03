029679cc53c8387421b2ecb45ca3669c6c6eda6b
punitha86 <punith.job@gmail.com>
2020-01-03 12:14:29 -0800

    allow-origin header

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const MongoStore = require('connect-mongo')(session);
 const morgan = require('morgan');
 const keys=require('./config/keys');
 
-app.use('/*', function(req, res, next) {
+app.all('/*', function(req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
 res.header("Access-Control-Allow-Headers", "X-Requested-With");
 res.header('Access-Control-Allow-Headers', 'Content-Type');
