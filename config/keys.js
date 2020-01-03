10d505a0557ae9d23a17a940b505c90534e83efb
punitha86 <punith.job@gmail.com>
2020-01-02 23:58:47 -0800

    deployment bug fixes

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+
+
+module.exports={
+  google:{
+    clientID:'334754873002-5eegl23j77vv7jkn68tmugdcgnrfrasp.apps.googleusercontent.com',
+    clientSecret:'_e2E6HC8nT52lFT9RYjH0Mla'
+  },
+  sessionexp:{
+    sessionKey:'testingtheencryption'
+  }
+}
