21d45f914796852b73739d7b1876a4ce2e33151e
punitha86 <punith.job@gmail.com>
2020-01-06 22:19:02 -0800

    fixing deployment issues

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = mongoose.connection;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 //const PORT = 4500;
-require('dotenv').config();
+//require('dotenv').config();
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/family_travel';
 const passport = require('passport');
